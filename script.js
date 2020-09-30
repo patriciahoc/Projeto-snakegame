d2affbd859c8ee298c3aa63d4af708837640e88d
Patricia Hedra <patriciahedra@gmail.com>
2020-09-30 13:58:55 -0300

    Implementação do event listener

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 16;
+let box = 32;
 let snake = [];
 snake[0] = {
   x: 8 * box,
@@ -9,7 +9,7 @@ snake[0] = {
 let direction = "right";
 
 function criarBG(){
-  context.fillStyle = "lightgrenn";
+  context.fillStyle = "#90EE90";
   context.fillRect(0, 0, 16 * box, 16 * box);
 
 }
@@ -22,7 +22,24 @@ function criarSnaker(){
 
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+  if(event.keyCode == 37 && direction != "right") direction = "left";
+  if(event.keyCode == 38 && direction != "down") direction = "up";
+  if(event.keyCode == 39 && direction != "left") direction = "right";
+  if(event.keyCode == 40 && direction != "up") direction = "down";
+
+}
+
 function iniciarJogo(){
+
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * 0;
+  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+
+
   criarBG();
   criarSnaker();
 
@@ -46,3 +63,5 @@ function iniciarJogo(){
 }
 
 let jogo = setInterval(iniciarJogo, 100);
+
+
