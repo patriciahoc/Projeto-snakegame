8c9f06a6207078fa18cfd5b1ca909d4c2d383116
Patricia Hedra <patriciahedra@gmail.com>
2020-09-30 14:10:39 -0300

    Implementação da comida

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
   y: 8 * box
 }
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
   context.fillStyle = "#90EE90";
@@ -20,6 +24,11 @@ function criarSnaker(){
     context.fillRect(snake[i].x, snake[i]. y, box, box);
   }
 
+}
+
+function drawFood(){
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -42,6 +51,7 @@ function iniciarJogo(){
 
   criarBG();
   criarSnaker();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
